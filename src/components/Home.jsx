c41b4a6fc59b43d6b39be9ab7407c6a9bd45f8ff
Leonardo Sainz <leo.database92@gmail.com>
2021-05-18 18:31:37 -1200

    improved the get request, added component loader, message and helpHttp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { Navbar } from "./Navbar";
-import {urlApi} from '../constant/url'
+import { urlApi } from "../constant/url";
 import Item from "./Item";
-import { useFetch } from "../hooks/useFetch";
-
+import { helpHttp } from "../helpers/helpHttp";
+import { Message } from "./Message";
+import Loader from "./Loader";
 
 function Home() {
-  
-    let {data, isPending, error} = useFetch(urlApi)
-  
-    /*  const [data, setData] = useState();
-    useEffect(  
-        fetch(urlApi, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((res) => {
-          if (res) {
-            setData(res.title)
-          }
-        }), []);
- */
-console.log(data);
-    return (
-        <div className="container">
-            <Navbar/>
-           {!data ? (
-<><h1>nodata</h1></>
-             ):(
-                <>
-                  {data             
-                .map((el, index) => (
-                  <Item key={index} el={el} />
-                  
-                ))}
-                
-                </>
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-             )
-            }
-        </div>
-    )
+  useEffect(() => {
+    setLoading(true);
+    helpHttp().get(urlApi).then((res) => {
+      if (!res.err) {
+        setData(res);
+        setError(null);
+      } else {
+        setData(null);
+        setError(res);
+      }
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <Navbar />
+      {data === null ? (
+        <>
+          <h1>no data</h1>
+          {error && (
+            <>
+            <Message
+              msg={`Error ${error.status}: ${error.statusText}`}
+              bgColor="#dc3545"
+            />
+            </>
+          )}
+          {loading && <Loader />}
+        </>
+      ) : (
+        <>
+          {data.map((el, index) => (
+            <Item key={index} el={el} />
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
